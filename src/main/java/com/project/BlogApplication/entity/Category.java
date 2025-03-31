b26521a6eacb8188d9bfb50ca683d572442e5672
iamraguls <raguls4477@gmail.com>
2025-03-31 20:38:00 +0530

    added category entity

---
+++ src/main/java/com/project/BlogApplication/entity/Category.java
@@ -0,0 +1,41 @@
+package com.project.BlogApplication.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
