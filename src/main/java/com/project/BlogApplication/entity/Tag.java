7fc06135eece0e55e0cb413a0977a3092d5b40ad
iamraguls <raguls4477@gmail.com>
2025-03-31 20:40:37 +0530

    added tag entity

---
+++ src/main/java/com/project/BlogApplication/entity/Tag.java
@@ -0,0 +1,41 @@
+package com.project.BlogApplication.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
