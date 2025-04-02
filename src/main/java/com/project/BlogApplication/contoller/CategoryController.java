ebc920a92dc0ab9a89a9e6b1f0ff88031e3846b7
iamraguls <raguls4477@gmail.com>
2025-04-02 13:03:22 +0530

    added openapi-docs swagger api

--- src/main/java/com/project/BlogApplication/contoller/CategoryController.java
+++ src/main/java/com/project/BlogApplication/contoller/CategoryController.java
@@ -3,6 +3,8 @@ package com.project.BlogApplication.contoller;
 import com.project.BlogApplication.service.CategoryService;
 import com.project.BlogApplication.dto.CategoryRequestDTO;
 import com.project.BlogApplication.dto.CategoryResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/categories")
+@Tag(name = "Category", description = "Category API")
 public class CategoryController {
 
     @Autowired
@@ -23,16 +26,19 @@ public class CategoryController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all categories")
     public ResponseEntity<List<CategoryResponseDTO>> getAllCategories() {
         return ResponseEntity.ok(categoryService.getAllCategories());
     }
 
     @PostMapping
+    @Operation(summary = "add category")
     public ResponseEntity<CategoryResponseDTO> createCategory(@RequestBody CategoryRequestDTO category){
         return new ResponseEntity<>(categoryService.createCategory(category), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete category")
     public ResponseEntity<String> deleteCategory(@PathVariable UUID id){
         return new ResponseEntity<>(categoryService.deleteCategory(id), HttpStatus.OK);
     }
